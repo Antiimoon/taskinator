d75dc1b627e79b8e24d344c98a43c84d220fae67
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2021-11-30 18:32:51 -0800

    task form handled

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#task-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     taskToDoEl.apeendChild("listItemEl")
 }
 
-buttonEl.addEventListener("click", createTaskHandler); {
+formEl.addEventListener("click", createTaskHandler); {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
